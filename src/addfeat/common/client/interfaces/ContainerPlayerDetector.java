aa58f630cbb98182045c2b8241d77561220f21e1
tgame14 <tgameBusiness@gmail.com>
2013-10-30 17:29:52 +0200

    Revert "playerDetector inventory added"
    This reverts commit 2c2ef08d730d61d34032d54c72d28bcddfb25749.

--- src/addfeat/common/client/interfaces/ContainerPlayerDetector.java
+++ src/addfeat/common/client/interfaces/ContainerPlayerDetector.java
@@ -3,43 +3,19 @@ package addfeat.common.client.interfaces;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
-import net.minecraft.inventory.Slot;
-import net.minecraft.item.ItemStack;
 import addfeat.common.tileentities.TileEntityPlayerDetector;
 
 public class ContainerPlayerDetector extends Container {
 
 	private TileEntityPlayerDetector detector;
 
-	public ContainerPlayerDetector(InventoryPlayer invPlayer,
-			TileEntityPlayerDetector detector) {
+	public ContainerPlayerDetector(InventoryPlayer invPlayer, TileEntityPlayerDetector detector) {
 		this.detector = detector;
-
-		for (int i = 0; i < 9; i++) {
-			addSlotToContainer(new Slot(invPlayer, i, 8 + 18 * i, 130));
-		}
-
-		for (int j = 0; j < 3; j++) {
-			for (int i = 0; i < 9; i++) {
-				addSlotToContainer(new Slot(invPlayer, i + j * 9 + 9,
-						8 + 18 * i, 72 + j * 18));
-			}
-		}
-
-		for (int i = 0; i < 3; i++) {
-			addSlotToContainer(new Slot(detector, i, 8 + 18 * i, 17));
-		}
-
 	}
-
+	
 	@Override
 	public boolean canInteractWith(EntityPlayer entityplayer) {
 		return detector.isUseableByPlayer(entityplayer);
 	}
 	
-	@Override
-	public ItemStack transferStackInSlot(EntityPlayer par1EntityPlayer, int par2) {
-		return null;
-	}
-
 }
