b59a09a8bac7b0e462c94aae1b5775844caa86b5
tgame14 <tgameBusiness@gmail.com>
2013-11-16 16:58:12 +0200

    thatsIch Prettifies stuff!

--- src/addfeat/common/tileentities/TileEntityLiquidME.java
+++ src/addfeat/common/tileentities/TileEntityLiquidME.java
@@ -40,11 +40,11 @@ public class TileEntityLiquidME extends TileEntity implements IGridTileEntity {
 	}
 
 	private double getBiomeCustomTemp() {
-		double x = (double) (worldObj.getBiomeGenForCoords(xCoord, zCoord)
-				.getIntTemperature() / 65536);
-		if (x < 0.11)
+		final double temp = worldObj.getBiomeGenForCoords(xCoord, zCoord)
+				.getIntTemperature() / 65536.0;
+		if (temp < 0.11)
 			return 0.11;
-		return x;
+		return temp;
 	}
 
 	private void solidify() {
@@ -55,27 +55,26 @@ public class TileEntityLiquidME extends TileEntity implements IGridTileEntity {
 
 	}
 
-	private void checkSpread() {
-
-		if (timer == 0) {
-			for (ForgeDirection dir : ForgeDirection.VALID_DIRECTIONS) {
-				if (worldObj.getBlockTileEntity(xCoord + dir.offsetX, yCoord
-						+ dir.offsetY, zCoord + dir.offsetZ) != null) {
-					if (worldObj.getBlockTileEntity(xCoord + dir.offsetX,
-							yCoord + dir.offsetY, zCoord + dir.offsetZ) instanceof IGridTileEntity
-							&& !(worldObj.getBlockTileEntity(xCoord
-									+ dir.offsetX, yCoord + dir.offsetY, zCoord
-									+ dir.offsetZ) instanceof TileEntityLiquidME)) {
-						worldObj.setBlock(xCoord + dir.offsetX, yCoord
-								+ dir.offsetY, zCoord + dir.offsetZ,
-								BlockInfo.LIQUID_ME_ID, 0, 3);
-					}
-				}
-			}
-			timer = 1200;
-		}
-		timer--;
-	}
+	 private void checkSpread()
+     {
+             if ( timer == 0 )
+             {
+                     for ( ForgeDirection dir : ForgeDirection.VALID_DIRECTIONS )
+                     {
+                             final int x = this.xCoord + dir.offsetX;
+                             final int y = this.yCoord + dir.offsetY;
+                             final int z = this.zCoord + dir.offsetZ;
+                             final TileEntity te = this.worldObj.getBlockTileEntity( x, y, z );
+                            
+                             if (te != null && te instanceof IGridTileEntity && !(te instanceof TileEntityLiquidME) )
+                             {
+                                     this.worldObj.setBlock( x, y, z, BlockInfo.LIQUID_ME_ID, 0, 3 );
+                             }
+                     }
+                     timer = 1200;
+             }
+             timer--;
+     }
 
 	@Override
 	public WorldCoord getLocation() {
