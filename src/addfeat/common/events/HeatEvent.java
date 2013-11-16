9704dedcdddfad8214ab43aba8250d391d8f14ef
tgame14 <tgameBusiness@gmail.com>
2013-11-16 21:48:53 +0200

    Revert "fixed gitIgnore and reworking events and such"
    This reverts commit bcc6e62b215723c28005ce8c0eced0e368857df7.

--- src/addfeat/common/events/HeatEvent.java
+++ src/addfeat/common/events/HeatEvent.java
@@ -1,21 +1,14 @@
 package addfeat.common.events;
 
-import appeng.api.me.util.IGridInterface;
-import net.minecraft.tileentity.TileEntity;
+import java.util.EventObject;
+
 import net.minecraftforge.event.Event;
 
-public class HeatEvent extends Event {
-	
-	/* This event will fire when heat Reaches 25%, 66% and 90%
-	 * 
-	 * 
-	 */
-	private IGridInterface grid;
-	private TileEntity te;
-	
-	
-	
-	public HeatEvent() {
-		
+public class HeatEvent extends EventObject{
+
+	public HeatEvent(Object source) {
+		super(source);
+		// TODO Auto-generated constructor stub
 	}
+
 }
---
+++ src/addfeat/common/events/IHeatEventListener.java
@@ -0,0 +1,8 @@
+package addfeat.common.events;
+
+import java.util.EventObject;
+
+public interface IHeatEventListener {
+	public void handleHeatEventHandler(EventObject e);
+	
+}
