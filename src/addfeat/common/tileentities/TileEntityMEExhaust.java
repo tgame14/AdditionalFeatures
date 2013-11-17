29996eb1a4ca6b2be6d173961f1f4ed9936a1220
tgame14 <tgameBusiness@gmail.com>
2013-11-17 20:53:13 +0200

    The Heat Implementation.

--- src/addfeat/common/tileentities/TileEntityMEExhaust.java
+++ src/addfeat/common/tileentities/TileEntityMEExhaust.java
@@ -1,34 +1,113 @@
 package addfeat.common.tileentities;
 
-import appeng.api.WorldCoord;
-import appeng.api.events.GridTileLoadEvent;
-import appeng.api.events.GridTileUnloadEvent;
-import appeng.api.me.tiles.IGridMachine;
-import appeng.api.me.util.IGridInterface;
+import java.util.Random;
+
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
+import addfeat.common.fluids.Fluids;
+import appeng.api.Blocks;
+import appeng.api.TileRef;
+import appeng.api.WorldCoord;
+import appeng.api.events.GridTileLoadEvent;
+import appeng.api.events.GridTileUnloadEvent;
+import appeng.api.me.tiles.ICellContainer;
+import appeng.api.me.tiles.IGridMachine;
+import appeng.api.me.util.IGridInterface;
 
 public class TileEntityMEExhaust extends TileEntity implements IGridMachine {
 
 	private boolean powerStatus = true, networkReady = false;
 	private float heatPercent;
 	private IGridInterface grid;
+	private byte ticker;
+	private short meltTicker;
+	private boolean bareBones;
+	private WorldCoord w;
+
+	Random gen = new Random();
 
 	public TileEntityMEExhaust() {
 		this.heatPercent = 0;
+		this.ticker = 10;
+		this.meltTicker = 0;
 
 	}
 
 	@Override
 	public void updateEntity() {
 		if (!worldObj.isRemote && grid != null) {
-			heatPercent = (grid.getPowerUsageAvg() / 10) / 100;
-			
-			System.out.println("Heat % : " + heatPercent);
-			System.out.println("coldPower : " + (grid.getPowerUsageAvg() - getPowerDrainPerTick()));
 
+			heatPercent = (grid.getPowerUsageAvg() / 10) / 100;
+
+			/*
+			 * System.out.println("Heat % : " + heatPercent);
+			 * System.out.println("coldPower : " + (grid.getPowerUsageAvg() -
+			 * getPowerDrainPerTick()));
+			 */
+			if (ticker == 0) {
+				OnOverHeat();
+				ticker = 10;
+			}
+			ticker--;
 		}
+	}
+
+	private boolean isBareBones() {
+		for (int i = 0; i < grid.getMachines().size(); i++) {
+			if (!(isSafeFromMelt(grid.getMachines().get(i)))) {
+				System.out.println("barebonesfalse");
+				return false;
+			}
+		}
+		System.out.println("barebonetrue");
+		return true;
+	}
+
+	private boolean isSafeFromMelt(TileRef machine) {
+		final int x = machine.x;
+		final int y = machine.y;
+		final int z = machine.z;
+
+		final int machineBlockID = worldObj.getBlockId(x, y, z);
+		final int controllerID = Blocks.blkController.itemID;
+		final TileEntity te = worldObj.getBlockTileEntity(x, y, z);
+
+		if (machineBlockID == controllerID || te instanceof ICellContainer
+				|| te instanceof TileEntityMEExhaust) {
+			return true;
+		}
+		return false;
+	}
+
+	private void OnOverHeat() {
+		if (!(isBareBones())) {
+			WorldCoord w = getRandomAETile();
+
+			if (heatPercent >= 0.0 && meltTicker == 0) {
+				worldObj.destroyBlock(w.x, w.y, w.z, false);
+				worldObj.setBlock(w.x, w.y, w.z, Fluids.fluidME.getBlockID());
+				meltTicker = 800;
+			}
+
+			meltTicker--;
+
+			if (heatPercent >= 0.66) {
+
+			}
+
+			if (heatPercent >= 0.90) {
+
+			}
+		}
+
+	}
+
+	private WorldCoord getRandomAETile() {
+		TileRef machine = grid.getMachines().get(
+				gen.nextInt(grid.getMachines().size()));
+
+		return new WorldCoord(machine.x, machine.y, machine.z);
 	}
 
 	@Override
